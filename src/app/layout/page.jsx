7eaaf902e2b820c89093c220d7df3b3d79205e70
aalan <avalosalan789@gmail.com>
2024-03-04 14:47:47 -0600

    cambios en las librerias

--- src/app/layout/page.jsx
+++ src/app/layout/page.jsx
@@ -1,34 +1,35 @@
-import { Button, Container, Grid, Paper } from "@mui/material";
+import { Button, Container, Grid, Paper, Item } from "@mui/material";
 import React from "react";
 
 function LayoutPage() {
   return (
-    <div >
+    <div>
       <Grid container spacing={1}>
-        <Grid item xs={3}>
-          <Paper
-            sx={{
-              height: "15vh",
-              background: "red",
-            }}
-          />
-        </Grid>
-        <Grid item xs={9}>
-          <Paper
-            sx={{
-              height: "15vh",
-              background: "green",
-            }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Paper
-            sx={{
-              height: "85vh",
-              background: "blue",
-            }}
-          />
-        </Grid>
+        <Grid
+          item
+          xs={3}
+          sx={{
+            height: "15vh",
+            background: "red",
+          }}
+          
+        ></Grid>
+        <Grid
+          item
+          xs={9}
+          sx={{
+            height: "15vh",
+            background: "green",
+          }}
+        ></Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            height: "85vh",
+            background: "blue",
+          }}
+        ></Grid>
       </Grid>
     </div>
   );
