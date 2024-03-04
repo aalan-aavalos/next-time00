4d1b61cf1d57a5b7f0a3fb7a6493286db2d3238e
aalan <avalosalan789@gmail.com>
2024-03-03 18:07:43 -0600

    merge, dev/aalan -> test/main

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -4,15 +4,8 @@ function TestPage() {
   return (
     <div>
       <h1>Test</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <h1>Software developer in this project:</h1>
-=======
       <h2>Alan</h2>
->>>>>>> dev/aalan
-=======
-      <h2>Fernando</h2>
->>>>>>> dev/fer
     </div>
   );
 }
