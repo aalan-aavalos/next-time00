222ecbd4382fd055bc71d9eec8fa87c98bbbc79d
aalan <avalosalan789@gmail.com>
2024-03-10 10:20:15 -0600

    se agrego el cuadro de dialog

--- src/app/layout/page.jsx
+++ src/app/layout/page.jsx
@@ -1,4 +1,15 @@
-import { Grid, Button } from "@mui/material";
+"use client";
+
+import {
+  Grid,
+  Button,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  TextField,
+  DialogActions,
+  Dialog,
+} from "@mui/material";
 import React from "react";
 
 import Logo from "@/components/logo";
@@ -6,6 +17,16 @@ import NavBar from "@/components/navbar";
 import Lienzo from "@/components/lienzo";
 
 function LayoutPage() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div style={{ background: "#93A2B9" }}>
       <Grid container columnSpacing={2} rowSpacing={2} p={2}>
@@ -17,7 +38,54 @@ function LayoutPage() {
         </Grid>
         <Grid item xs={12}>
           <Lienzo>
-            <Button variant="contained">Agrear</Button>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              PaperProps={{
+                style: {
+                  background: "#93A2B9",
+                },
+                component: "form",
+                onSubmit: (event) => {
+                  event.preventDefault();
+                  const formData = new FormData(event.currentTarget);
+                  const formJson = Object.fromEntries(formData.entries());
+                  const email = formJson.email;
+                  console.log(email);
+                  handleClose();
+                },
+              }}
+            >
+              <DialogTitle>Subscribe</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  To subscribe to this website, please enter your email address
+                  here. We will send updates occasionally.
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  required
+                  margin="dense"
+                  id="name"
+                  name="email"
+                  label="Email Address"
+                  type="email"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button variant="contained" onClick={handleClose}>
+                  Cancel
+                </Button>
+                <Button variant="contained" type="submit">
+                  Subscribe
+                </Button>
+              </DialogActions>
+            </Dialog>
+            <Button variant="contained" onClick={handleClickOpen}>
+              Agrear
+            </Button>
           </Lienzo>
         </Grid>
       </Grid>
--- src/components/lienzo.jsx
+++ src/components/lienzo.jsx
@@ -3,15 +3,7 @@ import Box from "@mui/material/Box";
 
 function Lienzo({ children }) {
   return (
-    <Box
-      height={"80vh"}
-      bgcolor={"#2F3D5D"}
-      display="flex"
-      alignItems="center"
-      justifyContent={"center"}
-      p={2}
-      sx={{ borderRadius: 3 }}
-    >
+    <Box height={"80vh"} bgcolor={"#2F3D5D"} p={2} sx={{ borderRadius: 3 }}>
       {children}
     </Box>
   );
