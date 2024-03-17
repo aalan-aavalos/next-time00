6488862aedc096b71d74c51a2caf9fc8999d9d44
Karime-Caballero <137373510+Karime-Caballero@users.noreply.github.com>
2024-03-17 16:17:34 -0600

    ultima version modifique el modelo de areas

--- src/models/areas.js
+++ src/models/areas.js
@@ -2,22 +2,23 @@ const { Schema, model, models } = require("mongoose");
 
 const areasSchema = new Schema(
   {
-    aNombre: {
+    tipoArea: {
       type: String,
-      required: [true, "El nombre del area es requerido"],
-      //unique: [true, "El nombre no puede ser igual"], // Lo que hace es que manda un error si se repite
-      trim: true, // Lo que hace es que borra los espacios del final e inicio
-    },
-    sNombre: {
-      type: String,
-      required: [true, "La sede es requerida"],
+      required: [true, "El area es requerida"],
       //unique: true,
       trim: true,
     },
+    aNombre: {
+      type: String,
+      required: [true, "El nombre del area es requerido"],
+      unique: [true, "El nombre no puede ser igual"], // Lo que hace es que manda un error si se repite
+      trim: true, // Lo que hace es que borra los espacios del final e inicio
+    },
+   
   
   },
   {
-    timestamps: true /* Lo que hace es que agrega el campo de fecha de creacion y actualizacion */,
+    timestamps: true /* Lo que hace es que agrega el campo de fecha de creacion y actualizacionv  */,
   },
 );
 export default models.Areas || model("Areas", areasSchema);
