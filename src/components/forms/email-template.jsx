f19c277514fe2428f2a10e2a85eba97562959ac4
aalan <avalosalan789@gmail.com>
2024-04-06 12:32:42 -0600

    se agrego la confirmacion de correos correctament

--- src/components/forms/email-template.jsx
+++ src/components/forms/email-template.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export const EmailTemplate = ({ firstName, description }) => (
   <div>
-    <h1>Atencion, {firstName}!!!</h1>
+    <h1>Atencion, {firstName}!</h1>
 
     <p>{description}</p>
   </div>
