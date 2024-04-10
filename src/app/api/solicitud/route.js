d5cc92f9ac7813d5efcb8aa69b113f30e01571f1
aalan <avalosalan789@gmail.com>
2024-04-09 20:16:34 -0600

    a

--- src/app/api/solicitud/route.js
+++ src/app/api/solicitud/route.js
@@ -1,10 +1,23 @@
+// import Solicitudes from "@/models/solicitudes";
+// import { connectDB } from "@/utils/mongoose";
+// import { NextResponse } from "next/server";
+
+// // Obtener solicitudes
+// export async function GET() {
+//   connectDB();
+//   const solicitud = await Solicitudes.find();
+//   return NextResponse.json(solicitud);
+// }
+
+import Vacacion from "@/models/vacacion";
+
 import Solicitudes from "@/models/solicitudes";
 import { connectDB } from "@/utils/mongoose";
 import { NextResponse } from "next/server";
 
-// Obtener solicitudes
+// Obtener usuarios
 export async function GET() {
   connectDB();
-  const solicitud = await Solicitudes.find();
-  return NextResponse.json(solicitud);
+  const vacaciones = await Vacacion.find();
+  return NextResponse.json(vacaciones);
 }
