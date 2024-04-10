142dabca89c2b4ce0ded933341fecd2b784bf839
aalan <avalosalan789@gmail.com>
2024-04-09 20:38:22 -0600

    avr si ya

--- src/app/api/solic/route.js
+++ src/app/api/solic/route.js
@@ -1,10 +1,14 @@
-import Solicitudes from "@/models/turnos";
+import Solicitudes from "@/models/solicitudes";
 import { connectDB } from "@/utils/mongoose";
 import { NextResponse } from "next/server";
 
 // Obtener solicitudes
 export async function GET() {
-  connectDB();
-  const solicitud = await Solicitudes.find();
-  return NextResponse.json(solicitud);
+  try {
+    connectDB();
+    const solicitud = await Solicitudes.find();
+    return NextResponse.json(solicitud);
+  } catch (error) {
+    console.log(error)
+  }
 }
